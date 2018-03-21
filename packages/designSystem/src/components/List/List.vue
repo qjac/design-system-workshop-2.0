bb5d2d49e08c6fc8dee5db5b0b910ef52d860c33
Jacqueline Quintanilla <jac.quintanilla@gmail.com>
2018-03-21 14:53:50 -0500

    create list component

---
+++ packages/designSystem/src/components/List/List.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="List">
+  	<ul>	
+  	<li
+  		class="item"
+  		v-for="todo in data"
+  		:key="todo.id"
+  	>
+  		<slot name="item" :item="todo" />
+  	</li>
+  	</ul>
+  </div>
+</template>
+
+<script>
+export default {
+  components:{},
+  props: ["data"]
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../../utilities/utilities";
+
+.List {
+	margin:0;
+	padding:0;
+	list-style-type: none;
+	display: flex;
+	flex-direction:column;
+
+	item:not(:last-child) {
+		margin-bottom: 2rem;
+}
+}
+</style>
