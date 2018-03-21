f6cff3fe2dc5f2e0b059debe5d469902578be085
Jacqueline Quintanilla <jac.quintanilla@gmail.com>
2018-03-21 14:41:14 -0500

    create todo comonent

---
+++ packages/designSystem/src/components/TodoItem/TodoItem.stories.js
@@ -0,0 +1,21 @@
+import { storiesOf } from "@storybook/vue";
+import { action } from "@storybook/addon-actions";
+import {
+  withKnobs,
+  text,
+  number,
+  boolean,
+  array,
+  select,
+  color,
+  date
+} from "@storybook/addon-knobs";
+
+import myTodoItem from "./TodoItem.vue";
+
+storiesOf("TodoItem", module)
+  .addDecorator(withKnobs)
+  .add("Default", () => ({
+    components: { myTodoItem },
+    template: `<my-todoItem :todo="{id:0, title:'abc'}"> </my-todoItem>`
+  }));
---
+++ packages/designSystem/src/components/TodoItem/TodoItem.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="TodoItem">
+  <my-textfield
+  	variant="edit"
+  	:value="todo.title"
+  	@input="(value) => {todo.title = value}"
+  	@blur="$emit(`doneEdit`, todo)"
+  	@enter=$emit(`doneEdit`, todo)"
+  ></my-textfield>
+  <my-button
+  	class="delete"
+  	@click="$emit(`removeTodo`, todo)"
+  >x</my-button>
+  </div>
+</template>
+
+<script>
+import myButton from "../Button/Button.vue";
+import myTextfield from "../Textfield/Textfield.vue";
+
+export default {
+  components:{
+  	myButton,
+  	myTextfield
+  },
+  props: ["todo"]
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../../utilities/utilities";
+.TodoItem {
+	display: flex;
+	.delete {
+		display: none;
+	}
+	&:hover .delete {
+		display: block;
+	}
+}
+</style>
